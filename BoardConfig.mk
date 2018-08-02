5bb8802221be9e0a8adc89f6e997001eb867aac0
Jertlok <joseph96tb@gmail.com>
2018-08-02 14:07:21 +0200

    angler: fix TARGET_COPY_OUT_VENDOR

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,7 +126,7 @@ TARGET_AUX_OS_VARIANT_LIST := angler
 
 TARGET_RECOVERY_FSTAB = device/huawei/angler/fstab.angler
 # write vendor modules to system
-TARGET_COPY_OUT_VENDOR := system
+TARGET_COPY_OUT_VENDOR := vendor
 TARGET_RELEASETOOLS_EXTENSIONS := device/huawei/angler
 
 BOARD_SEPOLICY_DIRS += \
