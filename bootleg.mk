a608020631979a9c216e9f0b42d7fe2e6192e068
Jertlok <joseph96tb@gmail.com>
2018-08-02 13:18:32 +0200

    angler: shishufy

--- bootleg.mk
+++ bootleg.mk
@@ -2,14 +2,14 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some Bootlegger stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_angler
+PRODUCT_NAME := bootleg_angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 
@@ -17,6 +17,6 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM6.171019.030.E1 4805388 release-keys"
-
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM6.171019.030.E1 4805388 release-keys" \
+    DEVICE_MAINTAINERS="Jertlok"
 BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM6.171019.030.E1/4805388:user/release-keys
